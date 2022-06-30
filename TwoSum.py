fb33ad72dc5e3a3062a7f345cb5a2b06040c201d
Ayushi Gupta <107073709+ayushigupta2409@users.noreply.github.com>
2022-06-30 23:42:14 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,6 +1,18 @@
+#brute force method (Tn=O(n^2),Sn()=O(1))
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0,len(nums)):
             for k in range(i+1,len(nums)):
                 if nums[i]+nums[k]==target:
                     return i, k
+#optimized (Tn()=O(n),Sn()=O(1))
+class Solution:
+    def twoSum(nums, target):
+	# mapping value:index in the dictionary
+	hash_map = {}
+	for i in range(len(nums)):
+	  tmp = target - nums[i]
+	  if tmp not in hash_map:
+		hash_map[nums[i]] = i
+	  else:
+		return [i, hash_map[tmp]]
