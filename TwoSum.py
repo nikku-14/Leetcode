e45f68b575db0d1579793861d223f8e4ae27d91f
Ayushi Gupta <107073709+ayushigupta2409@users.noreply.github.com>
2022-07-01 16:36:28 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -7,12 +7,12 @@ class Solution:
                     return i, k
 #optimized (Tn()=O(n),Sn()=O(1))
 class Solution:
-    def twoSum(nums, target):
-	# mapping value:index in the dictionary
-	hash_map = {}
-	for i in range(len(nums)):
-	  tmp = target - nums[i]
-	  if tmp not in hash_map:
-		hash_map[nums[i]] = i
-	  else:
-		return [i, hash_map[tmp]]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # mapping value:index in the dictionary
+        hash_map = {}
+        for i in range(len(nums)):
+          tmp = target - nums[i]
+          if tmp not in hash_map:
+            hash_map[nums[i]] = i
+          else:
+            return [i, hash_map[tmp]]
